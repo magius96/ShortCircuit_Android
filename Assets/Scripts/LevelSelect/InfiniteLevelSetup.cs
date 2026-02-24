e2456ceff53889ac1054f8be1eb745d4b9770805
Jason Yarber <jasonyarber@hotmail.com>
2026-02-24 15:11:58 -0500

    Updated Infinite level selection screen to use new GUI

--- Assets/Scripts/LevelSelect/InfiniteLevelSetup.cs
+++ Assets/Scripts/LevelSelect/InfiniteLevelSetup.cs
@@ -4,30 +4,26 @@ using System.Collections;
 using Assets.Scripts;
 using Assets.Scripts.Levels;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class InfiniteLevelSetup : MonoBehaviour
 {
-    private bool _initialized = false;
-    private bool _use3Phase = false;
-    private float _gridsize = 5f;
-    private float _minMoves = 2f;
-
-    private GUIStyle _headerStyle;
-    private GUIStyle _toggleStyle;
-    private GUIStyle _leftLabelStyle;
-    private GUIStyle _rightLabelStyle;
-    private GUIStyle _sliderStyle;
-    private GUIStyle _sliderButtonStyle;
-    private GUIStyle _buttonStyle;
+    public Toggle ThreePhaseToggle;
+    public Slider GridSizeSlider;
+    public Slider MinMovesSlider;
+    public TextMeshProUGUI GridSizeLabel;
+    public TextMeshProUGUI MinMovesLabel;
 
     public void Start()
     {
         Globals.Load();
+        Initialize();
     }
 
     public void Update()
     {
-        if (!_initialized) Initialize();
+        //if (!_initialized) Initialize();
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SoundManager.PlaySFX("ButtonClick");
@@ -37,92 +33,45 @@ public class InfiniteLevelSetup : MonoBehaviour
 
     private void Initialize()
     {
-        _use3Phase = Globals.InfiniteLevelUses3Phase;
-        _gridsize = Globals.InfiniteLevelGridSize;
-        _minMoves = Globals.InfiniteLevelMoves;
-
-        _headerStyle = new GUIStyle();
-        _headerStyle.fontSize = 30;
-        _headerStyle.fontStyle = FontStyle.Bold;
-        _headerStyle.normal.textColor = new Color(1f, 1f, 0f);
-        _headerStyle.alignment = TextAnchor.LowerCenter;
-
-        _toggleStyle = new GUIStyle();
-        _toggleStyle.normal.textColor = Color.white;
-        _toggleStyle.fontSize = 20;
-        _toggleStyle.alignment = TextAnchor.LowerLeft;
-        _toggleStyle.normal.background = Globals.MakeColorTex(new Color(1, 1, 1, 0));
-
-        _leftLabelStyle = new GUIStyle();
-        _leftLabelStyle.fontSize = 20;
-        _leftLabelStyle.normal.textColor = Color.white;
-        _leftLabelStyle.alignment = TextAnchor.LowerLeft;
-
-        _rightLabelStyle = new GUIStyle();
-        _rightLabelStyle.fontSize = 20;
-        _rightLabelStyle.normal.textColor = Color.white;
-        _rightLabelStyle.alignment = TextAnchor.LowerRight;
-
-        _sliderStyle = new GUIStyle();
-        _sliderStyle.normal.background = Globals.MakeColorTex(Color.grey);
-        _sliderStyle.margin = new RectOffset(10, 10, 10, 10);
-
-        _sliderButtonStyle = new GUIStyle();
-        _sliderButtonStyle.normal.background = Globals.MakeColorTex(Color.white);
-        _sliderButtonStyle.padding = new RectOffset(10, 10, 10, 10);
-        _sliderButtonStyle.margin = new RectOffset(5, 5, 5, 5);
-
-        _buttonStyle = new GUIStyle();
-        _buttonStyle.normal.background = Globals.MakeColorTex(Color.blue);
-        _buttonStyle.fontSize = 22;
-        _buttonStyle.normal.textColor = Color.white;
-        _buttonStyle.fontStyle = FontStyle.Bold;
-        _buttonStyle.alignment = TextAnchor.MiddleCenter;
+        ThreePhaseToggle.isOn = Globals.InfiniteLevelUses3Phase;
+        GridSizeSlider.value = Globals.InfiniteLevelGridSize;
+        MinMovesSlider.value = Globals.InfiniteLevelMoves;
+        GridSizeLabel.text = Globals.InfiniteLevelGridSize.ToString();
+        MinMovesLabel.text = Globals.InfiniteLevelMoves.ToString();
     }
 
-    public void OnGUI()
+    public void ThreePhaseToggled()
     {
-        float rx = Screen.width / Globals.NativeWidth;
-        float ry = Screen.height / Globals.NativeHeight;
-        GUI.matrix = Matrix4x4.TRS(new Vector3(0, 0, 0), Quaternion.identity, new Vector3(rx, ry, 1));
-
-        GUI.Label(new Rect(0, 0, 320, 40), "Infinite Levels", _headerStyle);
-
-        if (GUI.Button(new Rect(10, 100, 300, 20), "Use 3 phase lights", _toggleStyle))
-        {
-            SoundManager.PlaySFX("ButtonClick");
-            _use3Phase = !_use3Phase;
-            Globals.InfiniteLevelUses3Phase = _use3Phase;
-        }
-        GUI.Label(new Rect(270, 100, 40, 20), _use3Phase ? "Yes" : "No", _rightLabelStyle);
-
-        GUI.Label(new Rect(10, 170, 200, 20), "Grid Size", _leftLabelStyle);
-        GUI.Label(new Rect(250, 170, 50, 20), _gridsize.ToString(), _rightLabelStyle);
-        _gridsize = GUI.HorizontalSlider(new Rect(10, 200, 300, 20), _gridsize, 3f, 10f, _sliderStyle, _sliderButtonStyle);
-        Globals.InfiniteLevelGridSize = (int)_gridsize;
-
-        GUI.Label(new Rect(10, 260, 200, 20), "Minimum Moves", _leftLabelStyle);
-        GUI.Label(new Rect(250, 260, 50, 20), _minMoves.ToString(), _rightLabelStyle);
-        _minMoves = GUI.HorizontalSlider(new Rect(10, 290, 300, 20), _minMoves, 1f, (_gridsize * _gridsize) / 2f, _sliderStyle, _sliderButtonStyle);
-        Globals.InfiniteLevelMoves = (int)_minMoves;
-
-        if (GUI.Button(new Rect(10, 430, 320, 40), "Start", _buttonStyle))
-        {
-            SoundManager.PlaySFX("ButtonClick");
-            Globals.PreviousScreen.Push("InfiniteLevelSetupScreen");
-            Globals.CurrentLevel = new InfiniteLevel();
-            SceneManager.LoadScene("GameScreen");
-        }
+        SoundManager.PlaySFX("ButtonClick");
+        Globals.InfiniteLevelUses3Phase = ThreePhaseToggle.isOn;
     }
 
-    private GUIStyle SetupButtonStyle(Color buttonColor)
+    public void GridSizeChanged()
     {
-        var style = new GUIStyle();
-        style.normal.background = Globals.MakeColorTex(buttonColor);
-        style.fontSize = 22;
-        style.alignment = TextAnchor.MiddleCenter;
-        style.fontStyle = FontStyle.Bold;
-        style.normal.textColor = Color.gray;
-        return style;
+        Globals.InfiniteLevelGridSize = (int)GridSizeSlider.value;
+        GridSizeLabel.text = Globals.InfiniteLevelGridSize.ToString();
+        MinMovesSlider.minValue = 1;
+        MinMovesSlider.maxValue = (int)((Globals.InfiniteLevelGridSize * Globals.InfiniteLevelGridSize) / 2f);
+        if (MinMovesSlider.value > MinMovesSlider.maxValue) MinMovesSlider.value = MinMovesSlider.maxValue;
+    }
+
+    public void MinMovesChanged()
+    {
+        Globals.InfiniteLevelMoves = (int)MinMovesSlider.value;
+        MinMovesLabel.text = Globals.InfiniteLevelMoves.ToString();
+    }
+
+    public void StartButtonClicked()
+    {
+        SoundManager.PlaySFX("ButtonClick");
+        Globals.PreviousScreen.Push("InfiniteLevelSetupScreen");
+        Globals.CurrentLevel = new InfiniteLevel();
+        SceneManager.LoadScene("GameScreen");
+    }
+
+    public void BackButtonClicked()
+    {
+        SoundManager.PlaySFX("ButtonClick");
+        Globals.LoadPreviousScreen();
     }
 }
